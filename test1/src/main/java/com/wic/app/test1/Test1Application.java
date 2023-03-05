40802d49abe7284b12054c2377105c3311440797
wence <wence@host.docker.internal>
2023-03-05 01:16:01 +0100

    import inicial

---
+++ test1/src/main/java/com/wic/app/test1/Test1Application.java
@@ -0,0 +1,13 @@
+package com.wic.app.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1Application.class, args);
+		System.out.println("test1!!!");
+	}
+}
